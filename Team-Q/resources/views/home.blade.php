a8cf4085e17c4a3411b5e852de2096ef05f41035
mfadam786 <mfadam786@gmail.com>
2020-08-06 12:21:46 +1200

    Changed fields needed to register

--- Team-Q/resources/views/home.blade.php
+++ Team-Q/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in, {{ Auth::user()->name }} !
+                    You are logged in, {{ Auth::user()->first_name }} !
                 </div>
             </div>
         </div>
