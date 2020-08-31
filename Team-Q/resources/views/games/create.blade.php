f53e6729f4eecde45ad45f4e29ca749d9d094e40
Isaac Thomas <50122666+iscthomas@users.noreply.github.com>
2020-08-31 21:41:37 +1200

    small fixes

--- Team-Q/resources/views/games/create.blade.php
+++ Team-Q/resources/views/games/create.blade.php
@@ -48,7 +48,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Image (Max 2MB):</strong>
-                <input id="image" type="file" class="form-control" name="image" required>
+                <input id="profile_image" type="file" class="form-control" name="image" required>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
